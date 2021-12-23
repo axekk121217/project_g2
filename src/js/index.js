7712806a17ddea3671eb0687c40e1e72c2d33dd7
LAPTOP-6IR4E4AF\Tibame_T14 <jerry61503@gmail.com>
2021-12-23 10:11:34 +0800

    add index ideas

---
+++ src/js/index.js
@@ -0,0 +1,61 @@
+function doFirst(){
+    // 跟HTML物件產生關連，再建事件聆聽功能
+    function map(){
+        var map = L.map('map').setView([48.85, 2.35], 5);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([48.85, 2.35]).addTo(map)
+            // .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            // .openPopup();
+    }
+    map();
+
+
+    // const divWidth = document.querySelector(".index_slider_container");
+    // const leftBtn = document.querySelector(".fa-arrow-left");
+    // const item = document.querySelectorAll('.slider_item');
+
+    // console.log(divWidth);
+    // console.log(leftBtn);
+    // console.log(item);
+    // // let divWidth = document.getElementsByClassName("index_slider_container");
+    // // console.log(divWidth);
+    // leftBtn.addEventListener('click', function(){
+    //     console.log(leftBtn);
+    //     let newdivWidth = parseInt(divWidth).width();
+    //     console.log(newdivWidth);
+    // });
+
+
+    // ----- lottie 動畫 ----- //
+    lottie.loadAnimation({
+        wrapper: svgContainer,
+        animType: 'svg',
+        loop: true,
+        path: 'https://assets7.lottiefiles.com/packages/lf20_aiapd4ct.json'
+    });
+
+    lottie.loadAnimation({
+        wrapper: index_travel_svg,
+        animType: 'svg',
+        loop: true,
+        path: 'https://assets1.lottiefiles.com/packages/lf20_wofwoz8o.json'
+    });
+
+    // ---- 文字省略 (...) ---- //
+    // let len = 15; // 超過15個字以"..."取代
+    // $(".Text_len").each(function(i){
+    //     if($(this).text().length > len){
+    //         $(this).attr("title", $(this).text());
+    //         let text = $(this).text().substring(0,len-1) + "...";
+    //         $(this).text(text);
+    //     }
+    // });
+    
+
+}
+// 多個事件時，要先宣告load
+window.addEventListener('load', doFirst);
