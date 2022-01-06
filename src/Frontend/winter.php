bd54dfb34fdf215483410b4398a37ae943249298
pcshen0828 <michelleshen0828@gmail.com>
2022-01-06 16:24:28 +0800

    add vue + connect to db

---
+++ src/Frontend/winter.php
@@ -0,0 +1,13 @@
+<?php
+    include('./Lib/Util.php'); 
+    // include('./connection.php'); // 這是佩君的連線
+    
+    $sql = "select * from CAMPION.ITINERARY where CATEGORY = '冬令營'";    
+    $statement = getConn()->prepare($sql); // 預載
+    // $statement->bindValue(1, '%'.$name.'%'); // (第n個?, 值 || 變數)
+    $statement->execute(); // 執行
+
+    $data = $statement->fetchAll();
+    echo json_encode($data); // 打包成 json 格式
+
+?>
