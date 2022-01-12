7af4cbf1faa1e6ac9bc687cdc666c5d980a76529
a0955307826 <a0955307826@gmail.com>
2022-01-12 20:12:30 +0800

    add suhe

--- src/Frontend/schedule.php
+++ src/Frontend/schedule.php
@@ -2,11 +2,11 @@
 include('../Lib/Util.php'); 
 
 $ID = $_GET['product_NAME'];
-$NAME = json_decode($ID);
+$number = json_decode($ID);
 
-$sql = "SELECT * FROM ITINERARY WHERE `NAME` = ?";
+$sql = "SELECT * FROM ITINERARY WHERE `ID` = ?";
 $statement = getPDO() -> prepare($sql);
-$statement -> bindValue(1, $NAME);
+$statement -> bindValue(1, $number);
 $statement -> execute();
 $data = $statement -> fetchAll();
 echo json_encode($data);
